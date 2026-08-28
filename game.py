16f9ac0dc9253cf63d13b5e9760f0363c5f597f6
AFKWOKE-lab <afkwoje@gamil.com>
2026-08-28 19:00:03 +0530

    game.py file added

---
+++ game.py
@@ -0,0 +1,11 @@
+import random
+
+a = random.randint(1,20)
+print("It was a random number finding game \n\nclue: \nThe number was inside 1 and 20")
+ans = int(input(enter your number))
+
+if(ans = a):
+  print("Wow congrats you win")
+else:
+  print(" ha I knew it")
+  print("the answer is ",a)
